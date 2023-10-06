1428e8dc633e3509b7d0e60f06056df13b6d0bd4
David Pivovar <pivovar.david@gmail.com>
2023-10-06 16:10:18 +0200

    prompt

--- add_profile.ps1
+++ add_profile.ps1
@@ -1,5 +1,8 @@
-$d = Split-Path -parent $profile
-cp ./Microsoft.PowerShell_profile.ps1 -Destination $d
+$confirmation = Read-Host "Do you want to upload a new profile? Old profile will be overwritten! (y/n)"
+if ($confirmation -eq 'y') {
+    $d = Split-Path -parent $profile
+	cp ./Microsoft.PowerShell_profile.ps1 -Destination $d
 
-echo "New profile copied:"
-cat $profile
+	echo "New profile copied:"
+	cat $profile
+}
